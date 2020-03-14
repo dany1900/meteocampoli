2fcb9ac3698608daf766fc1f123ecb67235012ca
DConflitti <cdany1900@gmail.com>
2020-03-14 11:12:48 +0100

    refactoring posizione + nome project + adeguamento a modifiche di altervista + tab lazy loading su immagini

--- meteocampoli/src/app/shared/constants/stazioni-meteo.constants.ts
+++
@@ -1,9 +0,0 @@
-export class StazioniMeteoCostants {
-  public static GENERALE: number = 1  ;
-  public static LAZIO: number = 2;
-  public static ABRUZZO: number = 3;
-  public static MOLISE: number = 4;
-  public static RETENAZIONALE: number = 5;
-  public static METEONETWORK: number = 6;
-
-}
