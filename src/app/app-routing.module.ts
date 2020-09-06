1e7623e570b62f482a24b52a7282bd66517961bc
DConflitti <cdany1900@gmail.com>
2020-09-06 14:38:11 +0200

    redirect prediposto + fix varie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,7 +15,30 @@ export const routes: Routes = [
   {path: 'webcam', loadChildren: './webcam/webcam.module#WebCamModule'},
   {path: 'terremoti', loadChildren: './terremoti/terremoti.module#TerremotiModule'},
   {path: 'info', loadChildren: './info/info.module#InfoModule'},
+  {path: 'index.htm', redirectTo: '/dati-attuali', pathMatch: 'full'},
+  {path: 'riepilogo.htm', redirectTo: '/riepilogo', pathMatch: 'full'},
+  {path: 'temperature-meteonetwork.html', redirectTo: '/stazioni-meteo/generale', pathMatch: 'full'},
+  {path: 'temperature.htm', redirectTo: '/stazioni-meteo/generale', pathMatch: 'full'},
+  {path: 'lazio.html', redirectTo: '/stazioni-meteo/lazio', pathMatch: 'full'},
+  {path: 'abruzzo.html', redirectTo: '/stazioni-meteo/abruzzo', pathMatch: 'full'},
+  {path: 'molise.html', redirectTo: '/stazioni-meteo/molise', pathMatch: 'full'},
+  {path: 'retemeteo.html', redirectTo: '/stazioni-meteo/rete-meteo', pathMatch: 'full'},
+  {path: 'thisyear.htm', redirectTo: '/previsioni', pathMatch: 'full'},
+  {path: 'images.htm', redirectTo: '/webcam/immagini', pathMatch: 'full'},
+  {path: 'webcam.htm', redirectTo: '/webcam/montagna/lazio', pathMatch: 'full'},
+  {path: 'webcam1.htm', redirectTo: '/webcam/limitrofe', pathMatch: 'full'},
+  {path: 'satellite.htm', redirectTo: '/satellite/generale', pathMatch: 'full'},
+  {path: 'satellite-centro.html', redirectTo: '/satellite/centro-italia', pathMatch: 'full'},
+  {path: 'satellite-nord.html', redirectTo: '/satellite/nord-italia', pathMatch: 'full'},
+  {path: 'satellite-sud.html', redirectTo: '/satellite/sud-italia', pathMatch: 'full'},
+  {path: 'terremoti.html', redirectTo: '/terremoti/italia', pathMatch: 'full'},
+  {path: 'terremoti-italia.html', redirectTo: '/terremoti/italia', pathMatch: 'full'},
+  {path: 'terremoti-mondo.html', redirectTo: '/terremoti/mondo', pathMatch: 'full'},
+  {path: 'terremoti-mondo.html', redirectTo: '/terremoti/mondo', pathMatch: 'full'},
+  {path: 'infos.htm', redirectTo: '/info/curiosita', pathMatch: 'full'},
+  {path: 'ondata-caldo-2017.html', redirectTo: '/info/curiosita/ondata-caldo-2017', pathMatch: 'full'},
   {path: '**', component: PageErrorComponent}
+
 ];
 
 @NgModule({
