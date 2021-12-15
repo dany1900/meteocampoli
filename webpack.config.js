97c6aee610e6050dabe5d714706a11bb3230fbf8
DConflitti <cdany1900@gmail.com>
2021-12-15 21:18:35 +0100

    update

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const glob = require('glob');
+const PurgeCSSPlugin = require('purgecss-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new PurgeCSSPlugin({
+      paths: glob.sync('./src/**/*.html', {nodir: true})
+    }),
+
+  ]
+};
