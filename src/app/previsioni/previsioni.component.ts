a6f2db85aa121d8f8bd61e3b736d587b0157825e
dconflitti <cdany1900@gmail.com>
2023-01-06 20:46:04 +0100

    update

--- src/app/previsioni/previsioni.component.ts
+++ src/app/previsioni/previsioni.component.ts
@@ -2,6 +2,7 @@ import {Component, OnInit, Renderer2} from '@angular/core';
 import {SEOService} from '../service/seoservice.service';
 import {Router} from '@angular/router';
 import {UtiliyService} from '../service/utiliy.service';
+import {DeviceDetectorService} from 'ngx-device-detector';
 
 @Component({
   selector: 'previsioni',
@@ -21,8 +22,11 @@ export class PrevisioniComponent implements OnInit {
   linkEcmwf: string;
   runGfs: string;
   runGem: string;
+  runEcmwf: string;
+  runEUkmo: string;
 
-  constructor(private seo: SEOService, protected router: Router, public renderer: Renderer2, public utilityService: UtiliyService) {
+
+  constructor(private seo: SEOService, protected router: Router, public renderer: Renderer2, public utilityService: UtiliyService, public deviceService: DeviceDetectorService) {
     this.title = 'Previsioni Meteo - Meteogrammi';
     this.description = 'Previsioni meteo dettagliate locali e nazionali. Analisi radio sondaggi e meteogrammi con tendenza a lungo termine.';
     this.keywords = 'previsioni meteo campoli, previsioni italia, meteogrammi, meteogramma gfs, meteogramma ecmwf, previsiono campoli 3bmeteo, previsioni campoli appennino, Tendenza meteo, previsioni meteo campoli appennino';
@@ -55,11 +59,11 @@ export class PrevisioniComponent implements OnInit {
     const month = (today.getMonth() + 1).toString();
     const day = String(today.getDate()).padStart(2, '0');
     const hours = today.getHours();
-    let emission = '00';
+    this.runEcmwf = '00';
     if (hours >= 21) {
-      emission = '12';
+      this.runEcmwf = '12';
     }
-    this.date = year + month + day + emission;
+    this.date = year + month + day + this.runEcmwf;
     return this.linkEcmwf = 'https://charts.ecmwf.int/products/opencharts_meteogram?base_time=' + this.date + '00&epsgram=classical_plume&lat=41.7357&lon=13.6837&station_name=Campoli%20Appennino';
   }
 
@@ -82,10 +86,10 @@ export class PrevisioniComponent implements OnInit {
     if (hours >= 19 || (hours >= 0 && hours < 1)) {
       this.runGfs = '12';
     }
-    if ((hours >= 0 && hours < 7) || hours >= 20) {
+    if ((hours >= 0 && hours < 7) || hours >= 19) {
       this.runGem = '12';
     }
-    if (hours >= 7 && hours <= 19) {
+    if (hours >= 7 && hours < 19) {
       this.runGem = '00';
     }
     return this.linkGfs = 'https://www.wetterzentrale.de/de/ens_image.php?geoid=75620&var=201&run=' + this.runGfs + '&date=' + year + '-' + month + '-' + day + '&model=gfs&member=ENS&bw=1';
